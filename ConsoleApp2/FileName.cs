094ef21385ecf33109363bc332b12d004d27ed8d
Sarthak12397 <ssarthak737@gmail.com>
2025-12-19 15:04:04 +0545

    changes

--- ConsoleApp2/FileName.cs
+++ ConsoleApp2/FileName.cs
@@ -20,9 +20,6 @@
 //    }
 //    else if (userGuess < secretnum)
 //    {
-
-//        Console.WriteLine("Too low");
-//    }
 //    else
 //    {
 
@@ -90,72 +87,131 @@
 
 // Text adventure game
 
-using System.Runtime.InteropServices;
-Console.WriteLine("Enter your class name");
-string classname = Console.ReadLine();
+//using System.Runtime.InteropServices;
+//Console.WriteLine("Enter your class name");
+//string classname = Console.ReadLine();
 
-Console.WriteLine("Do you want to enter the forest");
+//Console.WriteLine("Do you want to enter the forest");
 
-string choice = Console.ReadLine();
+//string choice = Console.ReadLine();
 
 
-if (choice.ToLower() == "enter")
+//if (choice.ToLower() == "enter")
+//{
+//    Console.WriteLine("you have entered the forest");
+//}
+//else
+//{
+//    Console.WriteLine("You are camping outside");
+
+//}
+
+
+//bool running = true;
+
+
+//while (running)
+//{
+//    Console.WriteLine("Which direction you want to take");
+
+//    string choices = Console.ReadLine();
+
+//    if(choices.ToLower() == "left")
+//    {
+//        Console.WriteLine("You have found the treasure");
+//        running = false;
+//    }
+//    else
+//    {
+//        Console.WriteLine("You have encountered the beast");
+//        Console.WriteLine("You have two option fight or flee");
+//        string option = Console.ReadLine();
+
+//        if(option.ToLower() == "fight")
+//        {
+//            Random randoms = new Random();
+//            int luck = randoms.Next(1, 11);
+
+//            if (luck > 5)
+//            {
+//                Console.WriteLine("You have damaged the beast");
+
+//                if (luck > 8)
+//                {
+//                    Console.WriteLine("You have dropped the treasure");
+
+//                }
+//            }
+//            else
+//            {
+
+//                Console.WriteLine("You are dead");
+
+
+//            }
+//            running = false;
+//        }
+//        Console.WriteLine("Thank you for playing the game");
+
+//    }
+//    Console.ReadKey();
+//}
+
+
+// Do while
+
+
+//int currentscore;
+//int sum = 0;
+//int counter = 0;
+//do
+//{
+//    Console.WriteLine("Enter -1 to finish");
+//    currentscore = int.Parse(Console.ReadLine());
+
+//    if (currentscore != -1)
+//    {
+//        sum += currentscore;
+//        counter++;
+//    }
+
+
+//}
+//while (currentscore != -1);
+
+//int average = sum / counter;
+//Console.WriteLine(average);
+
+
+
+//int score;
+//int sum = 0;
+//int counter = 0;
+//do
+//{
+//    Console.WriteLine("Enter - 1 to finish");
+//    score = int.Parse(Console.ReadLine());
+//    if (score == 0 || score == 1)
+
+//    {
+//        sum += score;
+//        counter++;
+//    }
+//} while (counter > 0);
+
+
+
+// Break and continue
+
+for(int i = 0; i < 10; i++)
 {
-    Console.WriteLine("you have entered the forest");
-}
-else
-{
-    Console.WriteLine("You are camping outside");
-
-}
-
-
-bool running = true;
-
-
-while (running)
-{
-    Console.WriteLine("Which direction you want to take");
-
-    string choices = Console.ReadLine();
-
-    if(choices.ToLower() == "left")
-    {
-        Console.WriteLine("You have found the treasure");
-        running = false;
-    }
-    else
-    {
-        Console.WriteLine("You have encountered the beast");
-        Console.WriteLine("You have two option fight or flee");
-        string option = Console.ReadLine();
-
-        if(option.ToLower() == "fight")
+    if(i == 3)
         {
-            Random randoms = new Random();
-            int luck = randoms.Next(1, 11);
-
-            if (luck > 5)
-            {
-                Console.WriteLine("You have damaged the beast");
-
-                if (luck > 8)
-                {
-                    Console.WriteLine("You have dropped the treasure");
-
-                }
-            }
-            else
-            {
-
-                Console.WriteLine("You are dead");
-                
-
-            }
-            running = false;
+            Console.WriteLine("Its enough");
+            continue;
         }
-        Console.WriteLine("Thank you for playing the game");
-        
-    }
-    Console.ReadKey();
+    Console.WriteLine("Whatever");
 }
+
+int[] ints = { 1, 2, 3, 4, 5 };
+Console.WriteLine(ints);
